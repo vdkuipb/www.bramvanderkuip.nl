894c6d5df429d146cef76af066a8af13c00e60e2
vdkuipb <vdkuipb@136-144-178-181.colo.transip.net>
2020-07-19 17:19:33 +0200

    Add projects based on github

--- src/projects/Project.js
+++ src/projects/Project.js
@@ -2,17 +2,45 @@ import React, { Component } from 'react';
 import './Project.css'
 
 class Project extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            repo: props.data
+        }
+
+        this.abortController = new window.AbortController();
+    }
+
+    componentDidMount() {
+        fetch(`https://raw.githubusercontent.com/${this.state.repo.full_name}/master/README.md`, { signal: this.abortController.signal })
+        .then(response => response.text())
+        .then(response => {
+            console.log(response);
+            this.setState({ 
+                about: response.match(new RegExp("<!--START-ABOUT-->(.*)<!--END-ABOUT-->"))[1],
+                thumb: response.match(new RegExp("<!--START-THUMB-->(.*)<!--END-THUMB-->"))[1],
+            });
+        });
+    }
+
+    componentWillUnmount() {
+        this.abortController.abort();
+    }
+
     render() {
+        console.log(this.state);
         return (
             <div className="Project">
-                <div className="ProjectThumb">
-                    <img alt="thumbnail of project" src="https://via.placeholder.com/150"></img>
-                </div>
+                <img className="ProjectThumb" alt="thumbnail of project" src={ this.state.thumb }></img>
                 <div className="ProjectTitle">
-                    <b>Title of Project</b>
+                    <b><a target="blank" href={ this.state.repo.html_url }>{ this.state.repo.name }</a></b>
+                    <div className="ProjectTitleDate">
+                        <i>{ this.state.repo.created_at.split("T")[0] } to { this.state.repo.pushed_at.split("T")[0] }</i>
+                     </div>
                 </div>
                 <div className="ProjectShortContent">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas bibendum, libero in suscipit tempus, metus quam egestas purus, sit amet tempus dui quam vitae purus. Maecenas sodales sed metus quis vehicula.
+                    { this.state.about }
                 </div>
             </div>  
         );
--- src/projects/ProjectList.js
+++ src/projects/ProjectList.js
@@ -3,13 +3,37 @@ import './ProjectList.css'
 
 import Project from './Project'
 
+const requestCache = "https://api.bramvanderkuip.nl/get/";
+
 class ProjectList extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            repos: []
+        }
+
+        this.abortController = new window.AbortController();
+    }
+
+    componentDidMount() {
+        fetch(`${requestCache}https://api.github.com/users/vdkuipb/repos`, { signal: this.abortController.signal })
+        .then(response => response.json())
+        .then(response => {
+            this.setState({ repos: response.data })
+        });
+    }
+
+    componentWillUnmount() {
+        this.abortController.abort();
+    }
+
     render() {
         return (
             <div className="ProjectList">
-                <Project></Project>
-                <Project></Project>
-                <Project></Project>
+                { this.state.repos.map((item, i) => (
+                        <Project data={item} key={i}></Project>
+                )) }
             </div>  
         );
     }
